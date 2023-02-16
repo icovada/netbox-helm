6310cca2b3f43da4665a76ba0359280ea9da7cb4
icovada <federico@tabbo.it>
2023-02-16 19:39:40 +0100

    Copy config with enabled plugins

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,5 @@ FROM netboxcommunity/netbox:v3.4
 COPY ./requirements.txt /
 RUN /opt/netbox/venv/bin/pip install  --no-warn-script-location -r /requirements.txt
 
-# These lines are only required if your plugin has its own static files.
-COPY configuration/configuration.py /etc/netbox/config/configuration.py
+COPY ./configuration/plugins.py /etc/netbox/config/.
 RUN SECRET_KEY="dummy" /opt/netbox/venv/bin/python /opt/netbox/netbox/manage.py collectstatic --no-input
---
+++ configuration/plugins.py
@@ -0,0 +1,16 @@
+# Add your plugins and plugin settings here.
+# Of course uncomment this file out.
+
+# To learn how to build images with your required plugins
+# See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
+# from os import environ
+
+PLUGINS = ["netbox_topology_views"]
+
+# PLUGINS_CONFIG = {
+#     'netbox_cisco_support': {
+#         'cisco_client_id': environ.get('CISCO_CLIENT_ID', '346ngz68yxdcynkanj8bsmqm'),
+#         'cisco_client_secret': environ.get("CISCO_CLIENT_SECRET", 'FA5dt47eWtfgFmQUzNjQZbRX')
+#     }
+# }
+
